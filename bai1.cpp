e7049a6d60625a10dc399aa519d8ee15950b0b61
KJDBVCW <156693611+KJDBVCW@users.noreply.github.com>
2024-02-22 08:50:30 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string a = 'table'; 
+    
+    string b;
+    for (int i = 0; i < a.length(); i++)
+    b = b + '-';
+    cout << b << endl;
+    int so_lan = 10;
+    string c;
+    while (so_lan > 0 && b != a) {
+
+    }
+    return 0;
+}
